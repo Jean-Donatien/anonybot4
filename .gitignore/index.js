7be08579b5c19527d42247d68b4424e73c253947
Jean-Donatien <38259101+Jean-Donatien@users.noreply.github.com>
2018-07-10 18:26:59 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,4 +1,10 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 
+
+bot.on("message", function(message) {
+  bot.channels.get('361975566565638146').sendMessage(message)
+});
+
+
 bot.login('NDY2MjcyMTkyODg0MTEzNDA4.DiZpWg.93nl-a6JYBUJLrAypujisqPXzQw')
